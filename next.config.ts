9a8f7707d1aba8f4ac0d2f943434b5dbc9b2effc
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2026-09-04 07:33:25 +0500

    style: update globals.css styling and configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  devIndicators: false,
   images: {
     qualities: [75, 90, 92],
     remotePatterns: [
@@ -21,3 +22,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
